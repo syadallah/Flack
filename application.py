f3fa25df74783a359eb7852da3d76da7d8ae5ba6
syadallah <syadallah@gmail.com>
2020-04-22 17:26:30 -0400

    Add route to index.html
    index.html added to templates folder

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 import os
 
 from flask import Flask
-from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
+
 
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
