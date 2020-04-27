ca13a4233d5ab2c4a87a6db6c99231bbcc2afa85
syadallah <syadallah@gmail.com>
2020-04-27 17:43:41 -0400

    Connect to websocket

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // Connect to websocket
+      var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 })
