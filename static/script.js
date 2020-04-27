fbfae139c1aac8a05afecbc817623b6dfd5e94a3
syadallah <syadallah@gmail.com>
2020-04-27 17:46:35 -0400

    Prevents Enter key to submit all form inputs

--- static/script.js
+++ static/script.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Connect to websocket
+// Connect to websocket
       var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+// Prevents Enter key to submit all form inputs
+      const formList = document.querySelectorAll('.inputForm');
+      formList.forEach(form => {
+          form.addEventListener('submit', e => {
+              e.preventDefault();
+          });
+      })
+
+
 })
